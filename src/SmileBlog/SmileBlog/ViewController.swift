8fa09c528097a4a781ac7ed96b8f5b5e28878b06
Joons <elddy0948@gmail.com>
2022-12-06 21:26:19 +0900

    [Feat] : No storyboard set up

--- src/SmileBlog/SmileBlog/ViewController.swift
+++ src/SmileBlog/SmileBlog/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  SmileBlog
-//
-//  Created by 김호준 on 2022/12/02.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
-
 }
 

f1a0574fe689984dc6f79ae28583d92767146bbb
Joons <elddy0948@gmail.com>
2022-12-12 15:20:23 +0900

    [Feat] : implementation Post api

--- src/SmileBlogBE/Sources/App/routes.swift
+++ src/SmileBlogBE/Sources/App/routes.swift
@@ -2,11 +2,16 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
-    }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
+  app.get { req async throws in
+    try await req.view.render("index", ["title": "Hello Vapor!"])
+  }
+  
+  app.get("hello") { req async -> String in
+    "Hello, world!"
+  }
+  
+  app.post("api", "posts", use: { req -> EventLoopFuture<Post> in
+    let post = try req.content.decode(Post.self)
+    return post.save(on: req.db).map({ post })
+  })
 }

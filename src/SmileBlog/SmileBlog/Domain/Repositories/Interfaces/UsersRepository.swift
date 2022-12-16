a71de94070e822f104f4a51bbae2a01c91084145
Joons <elddy0948@gmail.com>
2022-12-16 15:54:16 +0900

    [Feat] : User Repository and UserCase

---
+++ src/SmileBlog/SmileBlog/Domain/Repositories/Interfaces/UsersRepository.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+protocol UsersRepository {
+  func create(user: User, completion: @escaping (Result<User, Error>) -> Void)
+  func fetchUser(userID: String, completion: @escaping (Result<User, Error>) -> Void)
+  func fetchUsers(completion: @escaping (Result<[User], Error>) -> Void)
+  func fetchUserPosts(userID: String, completion: @escaping (Result<[Post], Error>) -> Void)
+}
---
+++ src/SmileBlog/SmileBlog/Domain/UseCases/UsersUseCase.swift
@@ -0,0 +1,53 @@
+import Foundation
+
+final class DefaultUsersUseCase {
+  private let usersRepository: UsersRepository
+  
+  init(usersRepository: UsersRepository) {
+    self.usersRepository = usersRepository
+  }
+  
+  func create(user: User, completion: @escaping (Result<User, Error>) -> Void) {
+    self.usersRepository.create(user: user, completion: { result in
+      switch result {
+      case .success(let user):
+        completion(.success(user))
+      case .failure(let error):
+        completion(.failure(error))
+      }
+    })
+  }
+  
+  func fetchUser(userID: String, completion: @escaping (Result<User, Error>) -> Void) {
+    self.usersRepository.fetchUser(userID: userID, completion: { result in
+      switch result {
+      case .success(let user):
+        completion(.success(user))
+      case .failure(let error):
+        completion(.failure(error))
+      }
+    })
+  }
+  
+  func fetchUsers(completion: @escaping (Result<[User], Error>) -> Void) {
+    self.usersRepository.fetchUsers(completion: { result in
+      switch result {
+      case .success(let users):
+        completion(.success(users))
+      case .failure(let error):
+        completion(.failure(error))
+      }
+    })
+  }
+  
+  func fetchUserPosts(userID: String, completion: @escaping (Result<[Post], Error>) -> Void) {
+    self.usersRepository.fetchUserPosts(userID: userID, completion: { result in
+      switch result {
+      case .success(let posts):
+        completion(.success(posts))
+      case .failure(let error):
+        completion(.failure(error))
+      }
+    })
+  }
+}
